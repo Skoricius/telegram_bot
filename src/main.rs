371f3fa86df69ee2d022799f6a78c890f34fb4aa
Luka Skoric <luka.skoric@dreams-ai.com>
2022-05-18 22:59:18 +0100

    working version of pic sender

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,45 @@
 use clap::Parser;
 use std::env;
 use teloxide::prelude::*;
+use teloxide::types::InputFile;
 
 /// Simple telegram communicator
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Text to send
-    #[clap(short, long, default_value = "Hey!")]
+    #[clap(short, long, default_value = "Done!", global = true)]
     text: String,
 
     /// Flag to send a picture file
     #[clap(short, long)]
-    picture: bool,
+    photo: Option<String>,
 }
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
     let args = Args::parse();
-    let message = args.text;
 
     let token =
         env::var("TELEGRAM_TOKEN").expect("Define the token with: export TELEGRAM_TOKEN=<token>");
     let chat_id = env::var("TELEGRAM_CHAT_ID")
         .expect("Define the chat id with: export TELEGRAM_CHAT_ID=<chat_id>");
-    // let message = if args.len() > 1 {
-    //     args[1..].join(" ")
-    // } else {
-    //     String::from("Done!")
-    //     // String::from("Neist_point.jpg")
-    // };
-    if args.picture {
-        println!("ey")
-    } else {
-        println!("yes")
-    }
-
-    pretty_env_logger::init();
-    log::info!("Sending message...");
     let bot = Bot::new(token).auto_send();
 
-    bot.send_message(chat_id.clone(), &message).await.unwrap();
-
-    log::info!("Message sent...");
+    match args.photo {
+        Some(path) => {
+            log::info!("Sending photo...");
+            let photo = InputFile::file(path);
+            bot.send_photo(chat_id, photo)
+                .await
+                .expect("Can not find the photo!");
+            log::info!("Photo sent!");
+        }
+        None => {
+            log::info!("Sending message...");
+            bot.send_message(chat_id, &args.text).await.unwrap();
+            log::info!("Message sent!");
+        }
+    }
 }
